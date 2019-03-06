edeb6651aa57a8b488fae78322b4bc4bbb6e0f15
mkan88 <matthewkan88@gmail.com>
2019-03-06 16:51:03 +0000

    Changed axis to use scientific notation.

--- src/plot_results.py
+++ src/plot_results.py
@@ -6,6 +6,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import mpl_toolkits.mplot3d.axes3d as p3
 import matplotlib.animation as animation
+from matplotlib import ticker
 
 
 def update_lines(dataLines, lines):
@@ -33,14 +34,19 @@ def main(filename):
         z = particlepos_dict['%i'%i].iloc[:,2]
         ax.plot(x, y, z)
 
-    ax.set_xlim3d([-1e-6, 1e-6])
+    ax.set_xlim3d([-1e-5, 1e-5])
     ax.set_xlabel('x')
 
-    ax.set_ylim3d([-1e-6, 1e-6])
+    ax.set_ylim3d([-1e-5, 1e-5])
     ax.set_ylabel('y')
 
-    ax.set_zlim3d([-1e-6, 1e-6])
+    ax.set_zlim3d([-1e-5, 1e-5])
     ax.set_zlabel('z')
+
+    ax.xaxis.set_major_formatter(ticker.FormatStrFormatter('%.2e'))
+    ax.yaxis.set_major_formatter(ticker.FormatStrFormatter('%.2e'))
+    ax.zaxis.set_major_formatter(ticker.FormatStrFormatter('%.2e'))
+
 
     plt.show()
 
@@ -57,6 +63,6 @@ def main(filename):
 
 if __name__ == '__main__':
     if len(sys.argv) == 2:
-        main(sys.argv([1]))
+        main(sys.argv[1])
     else:
         main('../output0000000.csv')
